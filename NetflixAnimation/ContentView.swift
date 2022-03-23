0cbae1896fbe1de19194fc80ff49aaf8564fb71b
Anmol Verma <anmol.verma4@gmail.com>
2022-03-23 19:32:06 +0530

    Initial Commit

---
+++ NetflixAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NetflixAnimation
+//
+//  Created by Anmol Verma on 23/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetflixAnimation/NetflixAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetflixAnimationApp.swift
+//  NetflixAnimation
+//
+//  Created by Anmol Verma on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct NetflixAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
